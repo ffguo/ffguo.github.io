2ec686c41f29bf4e57ac6c56e2c9679573fbf228
ffguo <851501783@qq.com>
2017-10-03 18:31:21 +0800

    ADD QRCode

---
+++ ghostButton/QRCode/main.js
@@ -0,0 +1,14 @@
+﻿var data=['超超，我喜欢你','wacc520941','超超，我爱你','超超，520941'];
+
+window.onload=function ()
+{
+	beginfun();
+};
+
+
+function beginfun()
+{
+	var oTitle=document.getElementById('title'),
+		random=Math.floor(Math.random()*4);
+	oTitle.innerHTML=data[random];
+}
