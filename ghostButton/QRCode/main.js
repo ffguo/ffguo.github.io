7f2624f8103b69622aa5862d29c5fb7fc62968f9
Leon <851501783@qq.com>
2018-02-10 19:56:06 +0800

    修改图片显示

--- ghostButton/QRCode/main.js
+++ ghostButton/QRCode/main.js
@@ -1,4 +1,5 @@
-﻿var data=['超超，我喜欢你','wacc520941','超超，我爱你','超超，520941'];
+﻿//var data=['超超，我喜欢你','wacc520941','超超，我爱你','超超，520941'];
+var data = ['大哥好帅', '我是大哥的小迷妹', '大哥哥，要抱抱~'];
 
 window.onload=function ()
 {
@@ -9,6 +10,6 @@ window.onload=function ()
 function beginfun()
 {
 	var oTitle=document.getElementById('title'),
-		random=Math.floor(Math.random()*4);
+		random=Math.floor(Math.random()*data.length);
 	oTitle.innerHTML=data[random];
 }
