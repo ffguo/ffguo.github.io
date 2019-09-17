2ae673a51004ca35b8ba8a736bcfc6cc20d17603
ffg <851501783@qq.com>
2019-09-17 22:40:26 +0800

    解决结束后可以继续点击的BUG

--- ghostButton/pokeGame/js/main.js
+++ ghostButton/pokeGame/js/main.js
@@ -2,28 +2,30 @@ $(function (){
 	var $time = $('#time'),
 	$btn = $('#btn'),
 	status = 'init',
+	name = GetUrlParms().name,
 	time = 5000,
-	count =0;
+	count = 0
+	isEnd = false;
 
 	$btn.on('touchstart click', function (){
-		switch(status){
-			case 'init':
-				start();
-				break;
-			case 'started':
-				$btn.html(++count);
-				break;
+		if (!isEnd)
+		{
+			switch(status){
+				case 'init':
+					start();
+					break;
+				case 'started':
+					$btn.html(++count);
+					break;
+			}
+		}
+		else
+		{
+			alert(name + "请重新开始吧！");
 		}
 	})
 
 	function start(){
-		var url = location.href;
-		var name = "";
-		if(url.split('=').length === 2)
-		{
-			name = decodeURI(url.split('=')[1]);
-		}
-
 		status = 'started';
 		$btn.html(++count);
 		var counter = setInterval(timer, 7),
@@ -40,6 +42,7 @@ $(function (){
 			if(curTime == '0.000'){
 				$time.html('时间到');
 				//SetScore(count);
+				isEnd = true;
 				alert("恭喜" + name + "获得分数：" + count);
 			}else{
 				$time.html(curTime + '秒');
@@ -62,6 +65,22 @@ $(function (){
 	            alert("得分" + score + "，很遗憾，距离最高分" + maxScore + "还差" + (maxScore - score) + "分，继续努力哦！");
 	        }
 	    })
+	}
+
+	function GetUrlParms()
+	{
+		var args=new Object();   
+		var query=location.search.substring(1);//获取查询串   
+		var pairs=query.split("&");//在逗号处断开   
+		for(var  i=0;i<pairs.length;i++)   
+		{   
+			var pos=pairs[i].indexOf('=');//查找name=value   
+				if(pos==-1)   continue;//如果没有找到就跳过   
+				var argname=pairs[i].substring(0,pos);//提取name   
+				var value=pairs[i].substring(pos+1);//提取value   
+				args[argname]=decodeURI(value);//存为属性   
+		}
+		return args;
 	}
 })
 
